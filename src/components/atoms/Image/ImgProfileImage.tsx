cfb168509fe9952075330c50f29cce11729e1bb3
YSB_uncyclocity <41094016+uncyclocity@users.noreply.github.com>
2021-11-30 21:52:21 +0900

    Update ImgProfileImage.tsx

--- src/components/atoms/Image/ImgProfileImage.tsx
+++ src/components/atoms/Image/ImgProfileImage.tsx
@@ -4,26 +4,32 @@ import { FadeIn } from "../../../styles/keyframes/fade";
 type Image = {
   src: string;
   alt: string;
-  width: number;
-  height?: number;
 };
 
 const Styles = styled.div`
   opacity: 0;
   animation: 1s ease-in-out 0s ${FadeIn};
   animation-fill-mode: forwards;
+  img {
+    width: 100px;
+  }
+
+  @media screen and (max-width: 700px) {
+    img {
+      width: 60px;
+    }
+  }
 `;
 
 export default function ImgProfileImage() {
   const image: Image = {
     src: "https://media.discordapp.net/attachments/820998088889073685/913400010849267733/F10D031A-6EBF-440D-ACE7-C54A53E19F9F.jpg",
-    width: matchMedia("screen and (max-width: 700px").matches ? 60 : 100,
     alt: "프로필 사진",
   };
 
   return (
     <Styles>
-      <img src={image.src} width={image.width} alt={image.alt} />
+      <img src={image.src} alt={image.alt} />
     </Styles>
   );
 }
