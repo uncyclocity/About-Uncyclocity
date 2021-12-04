cab519adcb44758cf28b08faaa08687c7c5e145e
YSB_uncyclocity <41094016+uncyclocity@users.noreply.github.com>
2021-12-04 18:29:24 +0900

    Update TxtWelcomeScrollHint.tsx

--- src/components/atoms/text/TxtWelcomeScrollHint.tsx
+++ src/components/atoms/text/TxtWelcomeScrollHint.tsx
@@ -11,8 +11,8 @@ const Styles = styled.div`
 
 export default function TxtWelcomeScrollHint() {
   if (matchMedia("screen and (max-width: 700px)").matches) {
-    return <Styles>아래 버튼으로 페이지를 넘겨 보세요 ;)</Styles>;
+    return <Styles>하단의 버튼으로 페이지를 넘겨 보세요 ;)</Styles>;
   } else {
-    return <Styles>스크롤을 통해 페이지를 넘겨 보세요 ;)</Styles>;
+    return <Styles>스크롤을 내려 페이지를 넘겨 보세요 ;)</Styles>;
   }
 }
